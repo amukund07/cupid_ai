fc2d960fc46fe92d8105070f553667faa09119ba
Amritanshu Mukund <amukund007@gmail.com>
2026-01-19 05:36:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestRegressor
 df = pd.read_csv("crush_interest_full_dataset.csv")
 
 
-# Splitting data set
+
 X = df.drop(columns=["target"])
 Y = df["target"]
 
@@ -30,7 +30,7 @@ def predict_interest(single_input):
         if col not in input_df.columns:
             input_df[col] = 0
 
-    # Correct column order
+
     input_df = input_df[X_train.columns]
 
     score = model.predict(input_df)[0]
@@ -41,20 +41,7 @@ import joblib
 joblib.dump(model, "model.pkl")
 
 
-if __name__ == "__main__":
-
-    single_input = {
-        "initiates_conversation": 7,
-        "engagement": 8,
-        "quick_responses": 6,
-        "eye_contact": 7,
-        "joke_responses": 6,
-        "nervous": 2,
-        "stays_near_you": 8,
-        "helps_you": 7,
-        "smiles": 9,
-        "takes_you_out": 6
-    }
 
     score = predict_interest(single_input)
     print(f"Predicted Interest Score: {score:.2f}")
+
